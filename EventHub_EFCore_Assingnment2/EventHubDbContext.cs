41fcd3a755256b4d239c4b94e5da55b6978ef383
Fathy Hosny <ali01120939266@gmail.com>
2026-04-24 17:22:37 +0300

    EventHubDbContext

---
+++ EventHub_EFCore_Assingnment2/EventHubDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using EventHub_EFCore_Assingnment2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventHub_EFCore_Assingnment2
+{
+    public class EventHubDbContext : DbContext
+    {
+        public DbSet<Organizer> Organizers { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Attendee> Attendees { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Server=.;Database=EventHubDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+         
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
